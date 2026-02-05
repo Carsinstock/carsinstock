7a100642396227984f77fbcfa59b2adf7af981a0
CarsInStock <autoloanagent@gmail.com>
2026-02-05 22:36:43 +0000

    Stabilize app routing + blueprints; site loading cleanly

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,19 @@
 from flask import Blueprint, render_template
 
-main = Blueprint("main", __name__)
+main = Blueprint('main', __name__)
 
-@main.route("/")
+@main.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('index.html')
 
-@main.route("/salespeople")
+@main.route('/salespeople')
 def salespeople():
-    return render_template("salespeople.html")
+    return render_template('salespeople.html')
 
-@main.route("/customers")
+@main.route('/customers')
 def customers():
-    return render_template("customers.html")
+    return render_template('customers.html')
 
-@main.route("/search-cars")
+@main.route('/search-cars')
 def search_cars():
-    return render_template("search_cars.html")
+    return render_template('search_cars.html')
