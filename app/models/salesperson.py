95a986eb7c91f687a3c2a19e1e633a55f00f4e2f
CarsInStock <autoloanagent@gmail.com>
2026-03-11 10:41:43 +0000

    feat: banner photo drag-to-reposition with cover_photo_y_offset

--- app/models/salesperson.py
+++ app/models/salesperson.py
@@ -18,6 +18,7 @@ class Salesperson(db.Model):
     dealership_name = db.Column(db.String(200))
     dealership_address = db.Column(db.String(500))
     banner_template = db.Column(db.String(50))
+    cover_photo_y_offset = db.Column(db.Integer, default=50)
     status = db.Column(db.String(50), default='active')
     hired_at = db.Column(db.DateTime, default=datetime.utcnow)
     terminated_at = db.Column(db.DateTime)
