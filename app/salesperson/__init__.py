d46532a546500974da4b7308d4a9c3ae7c3a470f
CarsInStock <autoloanagent@gmail.com>
2026-04-16 11:32:01 +0000

    Just Listed - increase Powered by CarsInStock visibility

--- app/salesperson/__init__.py
+++ app/salesperson/__init__.py
@@ -355,7 +355,7 @@ def generate_social_ad_image():
             font_tiny = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 16)
         except:
             font_tiny = font_sm
-        jl_draw.text((W-30, 1058), 'Powered by CarsInStock', font=font_tiny, fill=(90, 100, 115), anchor='rm')
+        jl_draw.text((W-30, 1058), 'Powered by CarsInStock', font=font_sm, fill=(160, 170, 185), anchor='rm')
 
         buf = io.BytesIO()
         jl_img.save(buf, format='PNG')
